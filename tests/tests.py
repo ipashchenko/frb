ff932314af1eeeff55ec39ee4cfa8a825412db41
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-01-27 13:24:42 +0300

    now testing

--- tests/tests.py
+++ tests/tests.py
@@ -1,22 +1,22 @@
-from frb import antenna
+from frb.antenna import select_antenna
 import pytest
 
 
 def test_select_antenna():
     antenna = ['RADIO-AS', 'ZELENCHK', 'NOTO', 'USUDA64', 'SVETLOE', 'ARECIBO']
-    selected = antenna.select_antenna(antenna)
+    selected = select_antenna(antenna)
     assert selected == (['USUDA64'], ['NOTO', 'RADIO-AS'])
-    selected = antenna.select_antenna(antenna, ignored=['RADIO-AS'])
+    selected = select_antenna(antenna, ignored=['RADIO-AS'])
     assert selected == (['USUDA64'], ['NOTO', 'ZELENCHK'])
-    selected = antenna.select_antenna(antenna, n_big=2, n_small=3,
+    selected = select_antenna(antenna, n_big=2, n_small=3,
                                   ignored=['RADIO-AS'])
     assert selected == (['USUDA64', 'ARECIBO'], ['NOTO', 'ZELENCHK', 'SVETLOE'])
 
     with pytest.raises(Exception):
-        antenna.select_antenna(antenna, n_big=3)
+        select_antenna(antenna, n_big=3)
     with pytest.raises(Exception):
-        antenna.select_antenna(antenna, n_small=5)
+        select_antenna(antenna, n_small=5)
 
     antenna.append('SOME')
     with pytest.raises(KeyError):
-        antenna.select_antenna(antenna)
+        select_antenna(antenna)

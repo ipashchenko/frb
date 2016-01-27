8eb99a58623fa6322aff13da8ff3d5bd79b54bfb
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-01-27 13:51:37 +0300

    to package + pre-commit test run

---
+++ pre-commit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git stash -q --keep-index
+bash ./run_tests.sh
+RESULT=$?
+git stash pop -q
+[ $RESULT -ne 0 ] && exit 1
+exit 0
---
+++ setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+
+setup(
+    name='frb',
+    version='0.1',
+    author='Alexander Kutkin',
+    author_email='ikutkin@asc.rssi.ru',
+    packages=['frb', 'tests'],
+    scripts=[],
+    url='https://github.com/akutkin/frb',
+    license='LICENSE',
+    description='Search FRB in Radioastron data',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "numpy >= 1.7.2",
+        "scipy >= 0.12.0"
+    ],)

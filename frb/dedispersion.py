aa02bc4d153bb4bd3a909dd041de167ffeaccc17
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-02-01 15:41:57 +0300

    combaining 2 TDM arrays

--- frb/dedispersion.py
+++ frb/dedispersion.py
@@ -45,6 +45,7 @@ def de_disperse(dyn_spectr, nu_0, d_nu, d_t, dm_values):
 
     # Create array for TDM
     values = np.zeros((len(dm_values), n_t), dtype=float)
+    # FIXME: Generally there could be nonzero list of DM values
     # Fill DM=0 row
     values[0] = cumsums[-1]
 
@@ -57,5 +58,49 @@ def de_disperse(dyn_spectr, nu_0, d_nu, d_t, dm_values):
         for ix, j in enumerate(indx[:-1]):
             result += np.roll(dcumsums[j], -nt[j])
         values[i + 1] = result
+
+    return values
+
+
+def combain_tdm(tdm1, tdm2, nu_high1, d_nu1, nu_high2, d_t, dm_values):
+    """
+    Function that adds t-DM arrays from dedispersion of different frequency
+    bands.
+
+    :param tdm1:
+        2D array of de-dispersed values of first (high frequency) band.
+    :param tdm2:
+        2D array of de-dispersed values of second (low frequency) band.
+    :param nu_high1:
+        Highest frequency of first band.
+    :param d_nu1:
+        Frequency width of first band.
+    :param nu_high2:
+        Highest frequency of second band.
+    :param d_t:
+        Time step [s].
+    :param dm_values:
+        Array-like of DM values to de-disperse [cm^3 /pc].
+
+    :return:
+        2D TDM-array of de-dispersed values combined.
+    """
+    assert np.shape(tdm1) == np.shape(tdm2)
+    assert np.shape(tdm1)[0] == len(dm_values)
+
+    # Calculate shift of time caused by de-dispersion in first (high frequency)
+    # band for all values of DM
+    dt_all = k * dm_values * (1. / (nu_high1 + d_nu1) ** 2. -
+                              1. / nu_high1 ** 2.)
+    # Find what number of time bins corresponds to this shifts
+    nt_all = vint(vround(dt_all / d_t))
+
+    # Create array for TDM
+    values = np.zeros(np.shape(tdm1), dtype=float)
+
+    # Cycle over DM values and fill TDM array for others DM values
+    for i, nt in enumerate(nt_all):
+        # Find at which frequency channels time shifts have occurred
+        values[i] = tdm1[i] + np.roll(tdm2[i], -nt)
 
     return values

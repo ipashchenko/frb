9c075a0f8ccfc1a7ca7655ff74fe8dc40dacd743
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-02-04 13:33:50 +0300

    added vanilla noncoher.dedispersion

--- frb/dedispersion.py
+++ frb/dedispersion.py
@@ -1,4 +1,6 @@
 import numpy as np
+import ctypes
+import multiprocessing
 
 
 vint = np.vectorize(int)
@@ -108,3 +110,102 @@ def combain_tdm(tdm1, tdm2, nu_high1, d_nu1, nu_high2, d_t, dm_values):
         values[i] = tdm1[i] + np.roll(tdm2[i], -nt)
 
     return values
+
+
+    # FIXME: at small ``dt`` it uses too small DM-step for my laptop RAM:)
+def de_disperse_freq_average(params):
+    """
+    De-disperse frame using specified value of DM and average in frequency.
+    :param dm:
+        Dispersion measure to use in de-dispersion [cm^3 / pc].
+    :notes:
+        This method avoids creating ``(n_nu, n_t)`` arrays and must be
+        faster for data with big sizes. But it returns already frequency
+        averaged de-dispersed dyn. spectra.
+    """
+    dm, dsp, nu, nu_max, d_t = params
+    # MHz ** 2 * cm ** 3 * s / pc
+    k = 1. / (2.410331 * 10 ** (-4))
+    n_nu, n_t = dsp.shape
+
+    # Calculate shift of time caused by de-dispersion for all channels
+    dt_all = k * dm * (1. / nu ** 2. - 1. / nu_max ** 2.)
+    # Find what number of time bins corresponds to this shifts
+    nt_all = vint(vround(dt_all / d_t))
+    # Container for summing de-dispersed frequency channels
+    values = np.zeros(n_t)
+    # Roll each axis (freq. channel) to each own number of time steps.
+    for i in range(n_nu):
+        values += np.roll(dsp[i], -nt_all[i])
+
+    return values / n_nu
+
+
+def noncoherent_dedisperse(dsp, dm_grid, nu, nu_max, d_t, savefig=None, threads=1):
+    """
+    Method that de-disperse ``Frame`` instance with range values of
+    dispersion measures and average them in frequency to obtain image in
+    (t, DM)-plane.
+    :param dm_grid:
+        Array-like of value of DM on which to de-disperse [cm^3/pc].
+    :param savefig: (optional)
+        File to save picture.
+    :param threads: (optional)
+        Number of threads used for parallelization with ``multiprocessing``
+        module. If > 1 then it isn't used. (default: 1)
+    """
+    pool = None
+    if threads > 1:
+        pool = multiprocessing.Pool(threads, maxtasksperchild=1000)
+
+    if pool:
+        m = pool.map
+    else:
+        m = map
+
+    dsp_shared = to_shared_array(dsp)
+    dsp_shared += dsp
+
+    params = [(dm, dsp_shared, nu, nu_max, d_t) for dm in dm_grid]
+
+    # Accumulator of de-dispersed frequency averaged frames
+    frames = list(m(de_disperse_freq_average, params))
+    frames = np.array(frames)
+
+    if pool:
+        # Close pool
+        pool.close()
+        pool.join()
+
+    return frames
+
+
+def to_shared_array(array):
+        # Using shared array (http://stackoverflow.com/questions/5549190 by pv.)
+        i, j = array.shape
+        shared_array_base = multiprocessing.Array(ctypes.c_float, i * j)
+        return np.ctypeslib.as_array(shared_array_base.get_obj()).reshape((i,
+                                                                           j))
+
+if __name__ == '__main__':
+    from fits_io import get_dyn_spectr
+    idi_fits = '/mnt/frb_data/raw_data/re03jy/RE03JY_EF_C_AUTO.idifits'
+    t, nu_array, dsp = get_dyn_spectr(idi_fits, time=slice(0, 600000),
+                                      complex_indx=0, stokes_indx=0)
+    dsp += get_dyn_spectr(idi_fits, time=slice(0, 600000), complex_indx=0,
+                          stokes_indx=1)[2]
+    dsp *= 0.5
+    nu = np.reshape(nu_array, 128) / 10 ** 6
+    nu_max = np.max(nu_array.ravel()) / 10 ** 6
+    d_nu = (nu_array[0][1:] - nu_array[0][:-1])[0] / 10 ** 6
+    d_t = (t[1] - t[0]).sec
+    ddsp_kwargs = dict()
+    ddsp_kwargs.update({'nu_max': nu_max, 'd_nu': d_nu, 'd_t': d_t})
+    from frames import Frame
+    frame = Frame(128, len(t), nu_max, 0., 32./128, d_t)
+    frame.add_values(dsp)
+    for i in range(10):
+        frame.add_pulse(50. * (i + 1), 5. / 2. ** i, 0.003, dm=500.)
+    dm_grid = np.arange(0., 1000., 50.)
+    tdm = noncoherent_dedisperse(frame.values, dm_grid, nu, nu_max, d_t,
+                                 threads=4)

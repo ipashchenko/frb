eaf9b139cffd7a74bcfee324ed09e48400498bc2
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-05-13 20:19:57 +0300

    func for querying DB for FRBs
    Former-commit-id: 850d518799f95a22444126695c12d71f534e9d83

--- frb/candidates.py
+++ frb/candidates.py
@@ -1,7 +1,9 @@
-from sqlalchemy import Column, Integer, Float, String, ForeignKey, DateTime, Interval
+from sqlalchemy import (Column, Integer, Float, String, ForeignKey, DateTime)
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import backref, mapper, relation, sessionmaker
+from sqlalchemy.orm import (backref, relation, aliased, sessionmaker)
+from sqlalchemy.sql import func
+
 
 Base = declarative_base()
 
@@ -25,8 +27,9 @@ class SearchedData(Base):
     d_nu = Column(Float)
     nu_max = Column(Float)
 
-    def __init__(self, antenna, freq, band, pol, exp_code, algo, t_0=None,
-                 t_end=None, d_t=None, d_nu=None, nu_max=None):
+    def __init__(self, antenna=None, freq=None, band=None, pol=None,
+                 exp_code=None, algo=None, t_0=None, t_end=None, d_t=None,
+                 d_nu=None, nu_max=None):
         self.antenna = antenna
         self.freq = freq
         self.band = band
@@ -60,14 +63,16 @@ class Candidate(Base):
     dm = Column(Float)
     searched_data_id = Column(Integer, ForeignKey('searched_data.id'))
 
-    # creates a bidirectional relationship
-    # from Address to SearchedData it's Many-to-One
-    # from SearchedData to Address it's One-to-Many
     candidate = relation(SearchedData, backref=backref('candidates',
                                                        order_by=id))
 
     def __init__(self, t, dm):
-        """Constructor"""
+        """
+        :param t:
+            Instance of ``astropy.time.Time``.
+        :param dm:
+            Dispersion measure of pulse.
+        """
         self.t = t.utc.datetime
         self.dm = dm
 
@@ -78,45 +83,57 @@ class Candidate(Base):
 # create a connection to a sqlite database
 # turn echo on to see the auto-generated SQL
 engine = create_engine("sqlite:////home/ilya/code/akutkin/frb/frb/frb.db")
-                       # echo=True)
 
 # This creates tables
 metadata = Base.metadata
 metadata.create_all(engine)
 
-from sqlalchemy.orm import sessionmaker
-
 Session = sessionmaker(bind=engine)
 session = Session()
 session.commit()
 
-# Some examples commented out
-# some_searched_data = SearchedData("AR", "C", "U", "L", "raks3ar", "some_algo")
-# session.add(some_searched_data)
-# some_searched_data_too = SearchedData("WB", "L", "U", "L", "raks3az", "some_algo")
-# session.add(some_searched_data_too)
-# session.commit()
-#
-# some_searched_data.candidates = [Candidate("0.01", 500.),
-#                                  Candidate("0.41", 30.)]
-# some_searched_data_too.candidates = [Candidate("0.4", 100.)]
-# session.commit()
-#
-# all_searched_data = session.query(SearchedData).all()
-# print all_searched_data
-# all_candidates = session.query(Candidate).all()
-# print all_candidates
-#
-# from sqlalchemy.orm import join
-# sql = session.query(SearchedData).select_from(join(SearchedData, Candidate))
-# data = sql.filter(SearchedData.freq == 'L').filter(Candidate.dm > 50.).all()
-# print data
-# sql = session.query(Candidate).select_from(join(SearchedData, Candidate))
-# data = sql.filter(SearchedData.freq == 'L').filter(Candidate.dm > 50.).all()
-# print data
-#
-# # sql = session.query(SearchedData, Candidate)
-# # sql = sql.filter(SearchedData.id == Candidate.searched_data_id)
-# # sql = sql.filter(SearchedData.antenna == 'AR')
-# # for u, a in sql.all():
-#     print u, a
+
+def query_frb(exp_code, d_dm, d_t):
+    """
+    Function that queries DB for FRB events. Any 2 candidates with close DM & t
+    values on different antennas will be reported.
+
+    :param exp_code:
+        Code of experiment.
+    :param d_dm:
+        Maximum DM difference allowed for events at different antennas.
+    :param d_t:
+        Maximum time interval between events at different antennas [s].
+
+    :return:
+        List of tuples with ``(t1, t2, dm1, dm2, searched_data1,
+        searched_data2)``, where ``searched_data`` - instances of
+        ``Searched_data`` classes for corresponding candidates.
+    """
+    candidates1 = aliased(Candidate)
+    candidates2 = aliased(Candidate)
+    searched1 = aliased(SearchedData)
+    searched2 = aliased(SearchedData)
+
+    query = session.query(candidates1.t, candidates2.t, candidates1.dm,
+                          candidates2.dm, searched1, searched2).join((searched1,
+                                                candidates1.searched_data_id ==
+                                                searched1.id),
+                                               (candidates2,
+                                                searched1.id ==
+                                                candidates2.searched_data_id),
+                                               (searched2,
+                                                candidates2.searched_data_id ==
+                                                searched2.id)).\
+        filter(searched2.exp_code == exp_code). \
+        filter(searched1.exp_code == exp_code).\
+        filter(func.abs(candidates1.dm - candidates2.dm) < d_dm).\
+        filter(candidates1.t < candidates2.t). \
+        filter(func.abs(func.julianday(candidates2.t) -
+                        func.julianday(candidates1.t)) * 86400. < d_t)
+    data = query.all()
+    for row in data:
+        print row
+
+    return data
+

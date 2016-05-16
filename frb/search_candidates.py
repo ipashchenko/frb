e61d05bb69ba54c98107fa11dcdb73bbb6ba4b28
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-05-16 10:46:42 +0300

    docs for meta_data argument in Searcher constructor
    Former-commit-id: 42c9974aebc12bc7832208fe993098289c7bd4f6

--- frb/search_candidates.py
+++ frb/search_candidates.py
@@ -14,12 +14,14 @@ class Searcher(object):
         2D numpy array with dynamical spectra.
     :param meta_data:
         Dictionary with metadata describing current dynamical spectra. It must
-        include ``exp_name``, ``antenna``, ``freq``, ``band``, ``pol``, ``t_0``,
-        ``nu_max``, ``d_nu``, ``d_t`` keys.
+        include ``exp_name`` [string], ``antenna`` [string], ``freq`` [string],
+        ``band`` [string], ``pol`` [string], ``t_0`` [astropy.time.Time],
+        ``nu_max`` [number, MHz], ``d_nu`` [number, MHz], ``d_t`` [number, s]
+        keys.
 
         Eg. {'exp_name': 'raks03ra', 'antenna': 'AR'. 'freq': 'L', 'band': 'U',
-        'pol': 'L', 't_0': ``instance of astropy.time.Time``, 'nu_max': 1684.0,
-        'd_nu': 0.5, 'd_t': ``instance of astopy.time.TimeDelta``}
+        'pol': 'L', 't_0': ``instance of astropy.time.Time``, 'nu_max':
+        ``1684.0``, 'd_nu': ``0.5``, 'd_t': ``0.001``}
     """
     def __init__(self, dsp, meta_data):
         self.dsp = dsp
@@ -118,7 +120,7 @@ class Searcher(object):
             search_kwargs={}, preprocess_args=[], preprocess_kwargs={},
             db_file=None):
         """
-        
+
         :param de_disp_func:
             Function that used to de-disperse dynamical spectra.
         :param search_func:

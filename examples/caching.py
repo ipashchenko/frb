e9e351d7570b1590750b9e4a82a95288635c3c23
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-05-24 01:20:42 +0300

    improving clf-based search
    Former-commit-id: 4ca49cef693bc0a52eedc29fbb84365a77c6c6b0

--- examples/caching.py
+++ examples/caching.py
@@ -1,5 +1,5 @@
 import numpy as np
-from astropy.time import Time
+from astropy.time import Time, TimeDelta
 from frb.frames import create_from_txt
 from frb.search_candidates import Searcher
 from frb.dedispersion import de_disperse_cumsum
@@ -14,7 +14,7 @@ frame = create_from_txt(txt, 1684., 0, 16./128, 0.001)
 t0 = Time.now()
 print "Start time {}".format(t0)
 # Number of artificially injected pulses
-n_pulses = 10
+n_pulses = 20
 # Step of de-dispersion
 d_dm = 30.
 print "Adding {} pulses".format(n_pulses)
@@ -22,15 +22,17 @@ print "Adding {} pulses".format(n_pulses)
 # Set random generator seed for reproducibility
 np.random.seed(123)
 # Generate values of pulse parameters
-amps = np.random.uniform(0.25, 0.35, size=n_pulses)
+amps = np.random.uniform(0.15, 0.25, size=n_pulses)
 widths = np.random.uniform(0.001, 0.003, size=n_pulses)
 dm_values = np.random.uniform(100, 500, size=n_pulses)
 times = np.linspace(0.1, frame.shape[0] - 0.1, n_pulses)
 # Injecting pulses
 for t_0, amp, width, dm in zip(times, amps, widths, dm_values):
     frame.add_pulse(t_0, amp, width, dm)
-    print "Adding pulse with t0={:.3f}, amp={:.2f}, width={:.4f}," \
-          " dm={:.0f}".format(t_0, amp, width, dm)
+    t_1 = t0 + TimeDelta(t_0, format='sec')
+    print "Adding pulse with" \
+          " t0={:%Y-%m-%d %H:%M:%S.%f},".format(t_1.utc.datetime)[:-3] +\
+          " amp={:.2f}, width={:.4f}, dm={:.0f}".format(amp, width, dm)
 
 meta_data = {'antenna': 'WB', 'freq': 'L', 'band': 'U', 'pol': 'R',
              'exp_code': 'raks00', 'nu_max': 1684., 't_0': t0, 'd_nu': 16./128.,
@@ -43,41 +45,41 @@ searcher = Searcher(dsp=frame.values, meta_data=meta_data)
 
 # Run search for FRB with some parameters of de-dispersion, pre-processing,
 # searching algorithms
-print "using ``search_candidates`` search function..."
-candidates = searcher.run(de_disp_func=de_disperse_cumsum,
-                          search_func=search_candidates,
-                          preprocess_func=create_ellipses,
-                          de_disp_args=[dm_grid],
-                          search_kwargs={'n_d_x': 4., 'n_d_y': 15.,
-                                         'd_dm': d_dm},
-                          preprocess_kwargs={'disk_size': 3,
-                                             'threshold_big_perc': 97.5,
-                                             'threshold_perc': 98.5,
-                                             'statistic': 'mean'})
-print "Found {} candidates".format(len(candidates))
-for candidate in candidates:
-    print candidate
+# print "using ``search_candidates`` search function..."
+# candidates = searcher.run(de_disp_func=de_disperse_cumsum,
+#                           search_func=search_candidates,
+#                           preprocess_func=create_ellipses,
+#                           de_disp_args=[dm_grid],
+#                           search_kwargs={'n_d_x': 4., 'n_d_y': 15.,
+#                                          'd_dm': d_dm},
+#                           preprocess_kwargs={'disk_size': 3,
+#                                              'threshold_big_perc': 97.5,
+#                                              'threshold_perc': 98.5,
+#                                              'statistic': 'mean'})
+# print "Found {} candidates".format(len(candidates))
+# for candidate in candidates:
+#     print candidate
 
 # Run search for FRB with same parameters of de-dispersion, but different
 # pre-processing & searching algorithms
-print "using ``search_candidates_ell`` search function..."
-candidates = searcher.run(de_disp_func=de_disperse_cumsum,
-                          search_func=search_candidates_ell,
-                          preprocess_func=create_ellipses,
-                          de_disp_args=[dm_grid],
-                          search_kwargs={'x_stddev': 6., 'y_to_x_stddev': 0.3,
-                                         'theta_lims': [130., 180.],
-                                         'x_cos_theta': 3.,
-                                         'd_dm': d_dm,
-                                         'amplitude': 3.,
-                                         'save_fig': True},
-                          preprocess_kwargs={'disk_size': 3,
-                                             'threshold_big_perc': 97.5,
-                                             'threshold_perc': 98.5,
-                                             'statistic': 'mean'})
-print "Found {} candidates".format(len(candidates))
-for candidate in candidates:
-    print candidate
+# print "using ``search_candidates_ell`` search function..."
+# candidates = searcher.run(de_disp_func=de_disperse_cumsum,
+#                           search_func=search_candidates_ell,
+#                           preprocess_func=create_ellipses,
+#                           de_disp_args=[dm_grid],
+#                           search_kwargs={'x_stddev': 6., 'y_to_x_stddev': 0.3,
+#                                          'theta_lims': [130., 180.],
+#                                          'x_cos_theta': 3.,
+#                                          'd_dm': d_dm,
+#                                          'amplitude': 3.,
+#                                          'save_fig': True},
+#                           preprocess_kwargs={'disk_size': 3,
+#                                              'threshold_big_perc': 97.5,
+#                                              'threshold_perc': 98.5,
+#                                              'statistic': 'mean'})
+# print "Found {} candidates".format(len(candidates))
+# for candidate in candidates:
+#     print candidate
 
 print "using ``search_candidates_clf`` search function..."
 # ICreate classifier class instance
@@ -85,7 +87,7 @@ pclf = PulseClassifier(de_disperse_cumsum, create_ellipses,
                        de_disp_args=[dm_grid],
                        preprocess_kwargs={'disk_size': 3,
                                           'threshold_big_perc': 97.5,
-                                          'threshold_perc': 98.5,
+                                          'threshold_perc': 97.5,
                                           'statistic': 'mean'},
                        clf_kwargs={'kernel': 'rbf', 'probability': True,
                                    'class_weight': 'balanced'})
@@ -98,15 +100,15 @@ dsp.meta_data = {'antenna': 'WB', 'freq': 'L', 'band': 'U', 'pol': 'R',
 
 # Generate values of pulses in training sample
 print "Creating training sample"
-n_training_pulses = 25
+n_training_pulses = 50
 amps = np.random.uniform(0.15, 0.25, size=n_training_pulses)
-widths = np.random.uniform(0.001, 0.005, size=n_training_pulses)
+widths = np.random.uniform(0.001, 0.003, size=n_training_pulses)
 dm_values = np.random.uniform(100, 500, size=n_training_pulses)
-times = np.linspace(0, 20, n_training_pulses+2)[1: -1]
-features_dict, responces_dict = pclf.create_samples(dsp, amps, dm_values,
+times = np.linspace(0, 30, n_training_pulses+2)[1: -1]
+features_dict, responses_dict = pclf.create_samples(dsp, amps, dm_values,
                                                     widths)
-print "Training classifier"
-pclf.train(features_dict, responces_dict)
+# print "Training classifier"
+pclf.train(features_dict, responses_dict)
 
 print "Searching FRBs in actual data"
 # Note using the same arguments as in training classifier
